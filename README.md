ce50b33928dc49999676e81f0150ed680b438263
himym413 <95866956+himym413@users.noreply.github.com>
2022-09-16 23:18:14 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Expenses-Chart
-Frontend Mentor Challenge - Expenses Chart Component
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day’s bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device’s screen size
+- See hover states for all interactive elements on the page
+- **Bonus**: Use the JSON data file provided to dynamically size the bars on the chart
+
+### Screenshot
+
+![](./screenshot_mobile.jpg)
+![](./screenshot_desktop.jpg)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/himym413/Expenses-Chart)
+- Live Site URL: [Vercel](https://expenses-chart-kappa.vercel.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [Chart.js](https://www.chartjs.org/)
+
+## Author
+
+- Frontend Mentor - [@himym413](https://www.frontendmentor.io/profile/himym413)
---
+++ script.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const ctx = document.getElementById("myChart").getContext("2d");
+
+Chart.defaults.font.size = 10;
+Chart.defaults.font.family = "DM Sans, sans-serif";
+
+const tooltipTitle = function () {
+  return "";
+};
+
+const tooltipLabel = function (context) {
+  if (!context) return;
+
+  return "$" + context.raw;
+};
+
+// Background Color
+const setBackground = function (index, b = "background") {
+  if (b === "background")
+    return new Date().getDay() === index + 1
+      ? "hsl(186, 34%, 60%)"
+      : "hsl(10, 79%, 65%)";
+
+  if (b === "hover")
+    return new Date().getDay() === index + 1
+      ? "hsl(186, 50%, 80%)"
+      : "hsl(10, 95%, 80%)";
+};
+
+let backgroundArr = [];
+let hoverBackgroudArr = [];
+for (let i = 0; i < 7; i++) {
+  backgroundArr.push(setBackground(i));
+  hoverBackgroudArr.push(setBackground(i, "hover"));
+}
+
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+    datasets: [
+      {
+        data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+        backgroundColor: backgroundArr,
+        hoverBackgroundColor: hoverBackgroudArr,
+        borderRadius: "3",
+        borderSkipped: false,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        ticks: {
+          color: "hsl(28, 10%, 53%)",
+        },
+      },
+      y: {
+        display: false,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Spending - Last 7 Days",
+        color: "hsl(25, 47%, 15%)",
+        font: {
+          size: 24,
+          weight: 700,
+        },
+        align: "start",
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        backgroundColor: "rgba(0, 0, 0, 1)",
+        caretSize: 0,
+        padding: 8,
+        displayColors: false,
+        yAlign: "bottom",
+        callbacks: {
+          title: tooltipTitle,
+          label: tooltipLabel,
+        },
+      },
+    },
+    onHover: (event, chartElement) => {
+      event.native.target.style.cursor = chartElement[0]
+        ? "pointer"
+        : "default";
+    },
+  },
+});
